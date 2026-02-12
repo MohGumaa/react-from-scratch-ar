bf2a5b3618d80a05f58dbc99fd881f08974c6203
MohGuma <gomaam14@gamil.com>
2026-02-12 23:17:13 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Greet } from "./components/Greet"
+import Greeting from "./components/Greeting"
+
+function App () {
+  return (
+    <div>
+      {/* <Greet /> */}
+      <Greeting />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Greet.jsx
@@ -0,0 +1,9 @@
+// function Greet() {
+//   return <h1>Hello Ahmed</h1>
+// }
+
+export const Greet = () => {
+  return <h1>Hello Mohamed</h1> 
+}
+
+// export default Greet
