d757e0a8116a6b300668340b4991443749c926bb
MohGuma <gomaam14@gamil.com>
2026-04-12 13:54:54 +0400

    add todo list

--- rendering-lists-key/src/components/TaskList.jsx
+++ rendering-lists-key/src/components/TaskList.jsx
@@ -2,7 +2,7 @@ const TaskList = () => {
   const tasks = ["Learn React", "Build Project", "Deploy App"];
 
   return (
-    <div className='bg-gray-900 scheme-dark mx-auto max-w-7xl py-12 sm:px-6 lg:px-8 rounded-md border border-white/10'>
+    <div className='bg-gray-900 scheme-dark mx-auto max-w-3xl py-12 sm:px-6 lg:px-8 rounded-xl border border-white/10 shadow'>
       <div className="px-4 sm:px-0">
         <h3 className="text-base/7 font-semibold text-white">Task Information</h3>
         <p className="mt-1 max-w-2xl text-sm/6 text-gray-400">Task details and description.</p>
--- rendering-lists-key/src/components/TodoList.jsx
+++ rendering-lists-key/src/components/TodoList.jsx
@@ -1,9 +1,55 @@
 import { useState } from "react";
+import Todo from "../components/Todo"
 
 const TodoList = () => {
+  const [counter, setCounter] = useState(1);
+
+  const [list, setList] = useState([
+    {
+      id: 1,
+      createdAt: new Date(),
+    },
+  ]);
+
+  const addToStart = () => {
+    const nextId = counter + 1;
+
+    setList([{ id: nextId, createdAt: new Date() }, ...list]);
+    setCounter(nextId);
+  };
+
+  const addToEnd = () => {
+    const nextId = counter + 1;
+
+    setList([...list, { id: nextId, createdAt: new Date() }]);
+    setCounter(nextId);
+  };
+
   return (
-    <div>
-      TodoList
+    <div className="max-w-3xl mx-auto mt-10 p-6 bg-gray-900 shadow rounded-xl border border-white/10">
+      <h2 className="text-xl font-bold mb-4">Todo List</h2>
+
+      <div className="flex gap-3 mb-6">
+        <button
+          onClick={addToStart}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Add To Start
+        </button>
+
+        <button
+          onClick={addToEnd}
+          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+        >
+          Add To End
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        {list.map((todo, index) => (
+          <Todo key={index} index={index} {...todo} />
+        ))}
+      </div>
     </div>
   )
 }
