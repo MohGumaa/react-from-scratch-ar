5f4b5418311ab564583f621fc35cd86dcfca0e32
MohGuma <gomaam14@gamil.com>
2026-04-12 15:51:46 +0400

    update the button color

--- rendering-lists-key/src/App.jsx
+++ rendering-lists-key/src/App.jsx
@@ -1,11 +1,7 @@
-import TaskList from "./components/TaskList"
-import TodoList from "./components/TodoList"
-
 const App = () => {
   return (
     <>
-      {/* <TaskList/> */}
-      <TodoList/>
+      <h3 className="text-center text-4xl text-cyan-400">Readering list</h3>
     </>
   )
 }
